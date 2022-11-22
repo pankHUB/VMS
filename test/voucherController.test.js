ea5d98cc0bc1d98a23565f1e756e3fe36f6a8290
pankul-rathi <pankulrathi@gmail.com>
2022-11-22 12:29:50 +0530

    update

--- test/voucherController.test.js
+++ test/voucherController.test.js
@@ -2,34 +2,34 @@ const request = require("supertest");
 const app = require("../index");
 
 test("get all voucher api testing", async () => {
-  const response = await request(app).get("/api/users/get");
+  const response = await request(app).get("/api/voucher/get");
   expect(response.status).toBe(200);
 });
 
-test.skip("add all voucher api testing", async () => {
-  const response = await request(app).post("/api/users/add").send({
-    "type": "Knockout Deal",
+test("add all voucher api testing", async () => {
+  const response = await request(app).post("/api/voucher/add").send({
+    "type": "Gift Hampers",
     "Brand": "Pepper Fry",
-    "userId": "8"
+    "userId": "7"
 });
   expect(response.status).toBe(200);
 });
 
-test.skip("update all voucher api testing", async () => {
+test("update all voucher api testing", async () => {
   const response = await request(app)
-      .patch("/api/users/update")
+      .patch("/api/voucher/update")
       .send({
         "vaucherId": "76098f09-edbb-4a64-b994-09c300d386b6",
-        "type": "gift card",
+        "type": "Knockout Deal",
     });
   expect(response.status).toBe(200);
 });
 
-test.skip("This is delete voucher api testing", async () => {
+test("This is delete voucher api testing", async () => {
   const response = await request(app)
-    .delete("/api/users/delete")
+    .delete("/api/voucher/delete")
     .send({
-        "vaucherId": "a3eb835a-cd54-4732-bea8-20412d65873a",
+        "vaucherId": "e1497a9c-9d9a-46b8-9c97-a3a5c6dba236",
   });
   expect(response.status).toBe(200);
 });
